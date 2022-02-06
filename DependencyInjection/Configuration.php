1b7b33da81d97cf49988d34affe77a4c7204ea6f
Francesco Montanari <francesco.montanari@outlook.com>
2022-02-06 21:52:24 +0200

    Fix deprecation 'Method "ConfigurationInterface::getConfigTreeBuilder()" might add "TreeBuilder" as a native return type declaration in the future'

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('dubture_f_fmpeg');
         if (method_exists($treeBuilder, 'getRootNode')) {
